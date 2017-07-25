14be92eb1632c531e92bc897ecc44e1dc4a14e3d
Youssef-Darwish <yousef952009@hotmail.com>
2017-07-25 02:34:55 +0200

    Adding recycler view to show all habits
    Draft ... action is not added yet

--- app/src/main/java/com/example/android/habittracker/MainActivity.java
+++ app/src/main/java/com/example/android/habittracker/MainActivity.java
@@ -2,12 +2,78 @@ package com.example.android.habittracker;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.example.android.habittracker.data.Habit;
+
+import java.util.ArrayList;
+
+import static android.icu.lang.UCharacter.GraphemeClusterBreak.T;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private ArrayList<Habit> habitsList;
+    private RecyclerView mrecyclerview;
+    private HabitsAdapter mAdapter;
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        toolbar  =(Toolbar)findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        habitsList = new ArrayList<Habit>();
+        mrecyclerview = (RecyclerView) findViewById(R.id.recycler_view);
+        mAdapter  =  new HabitsAdapter(habitsList);
+        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getApplicationContext());
+        mrecyclerview.setLayoutManager(mLayoutManager);
+        mrecyclerview.setAdapter(mAdapter);
+
+        makeFakeData();
+
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.menu_main,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()){
+
+            case R.id.info_black_id:
+                Toast toast = new Toast(this);
+                toast.makeText(this,"info item was selected",Toast.LENGTH_LONG).show();
+                return true;
+
+            default:return super.onOptionsItemSelected(item);
+        }
+
+
+
+    }
+
+    private void makeFakeData(){
+        Habit h = new Habit("title1","description1","",0,0);
+        habitsList.add(h);
+
+        for (int i = 0; i<50; i++){
+            h = new Habit("title" + String.valueOf(i),"description" + String.valueOf(i),"",0,0);
+            habitsList.add(h);
+        }
+        mAdapter.notifyDataSetChanged();
     }
 }
